299f3ec2b955530a01f8cb87e8626ca154568a91
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-01-30 22:41:48 +0530

    Handled the asynchronous nature in node.js using promise

--- asnyc_feat_exp.js
+++ asnyc_feat_exp.js
@@ -6,3 +6,16 @@ setTimeout(() =>{
 })
 
 console.log("Ending.....")
+
+let a = 20;
+let b = 0;
+let waitingData = new Promise((resolve,reject) => {
+	setTimeout(() => {
+		resolve(30)
+	},2000)
+})
+
+waitingData.then((data) =>{
+	b = data;
+	console.log(a+b)
+})
