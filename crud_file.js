16a5258c39b2e8e2bd9ec504beccd9bd4f9550e0
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-01-30 22:23:11 +0530

    Crud operation with file is done

---
+++ crud_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const dirPath = path.join(__dirname,'crud')
+const filePath = `${dirPath}/apple.txt`
+const filePath1 = `${dirPath}/toDeleteapple.txt`
+
+// To create the file
+fs.writeFileSync(filePath,'This is a simple fruit file.')
+fs.writeFileSync(filePath1,'This is a simple fruit file.')
+
+// To read the file content
+fs.readFile(filePath,'utf8',(err,item) =>{
+	console.log(item)
+})
+
+// To update the file content
+fs.appendFile(filePath,'and file name is apple.txt',(err) =>{
+	if(!err) console.log("File is updated")
+})
+
+// To rename the file content
+fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
+	if(!err) console.log("File name is updated")
+})
+
+fs.unlinkSync(`${dirPath}/toDeleteapple.txt`,(err) =>{
+	if(!err) console.log("File is Deleted")
+})
+
