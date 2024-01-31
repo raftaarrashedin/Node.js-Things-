1a2c4307023b814432e94a16936b6d812d191410
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-01-31 11:13:55 +0530

    render html and json

--- exp_index.js
+++ exp_index.js
@@ -2,15 +2,27 @@ const express = require('express')
 const app = express()
 
 app.get('',(req,res) => {
-	res.send('Hello, this is Home page')
+	res.send(`
+		<h1>Welcome,MD RASHEDIN</h1>
+		<button><a href="/about"> Go to about Page</a></button>
+		`)
 });
 
 app.get('/about',(req,res) => {
-	res.send('Hello, this is About page')
+	res.send(`
+		<input type = "text" placeholder="write something" value = "${req.query.name}" />
+		<button>Click Here</button>
+
+
+		`);
+
 });
 
 app.get('/contact',(req,res) => {
-	res.send('Hello, this is Contact page')
+	res.send({
+		name : 'raftaar',
+		email : 'raftaar69@gmail.com'
+	})
 });
 
 app.listen(5000);
