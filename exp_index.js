b229137739c3fcba3afdc91c158a54908aa499c2
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-01-31 12:16:06 +0530

    made some html pages in public directory

--- exp_index.js
+++ exp_index.js
@@ -1,28 +1,9 @@
+
 const express = require('express')
+const path = require('path')
+
 const app = express()
+const publicPath = path.join(__dirname,'public')
 
-app.get('',(req,res) => {
-	res.send(`
-		<h1>Welcome,MD RASHEDIN</h1>
-		<button><a href="/about"> Go to about Page</a></button>
-		`)
-});
-
-app.get('/about',(req,res) => {
-	res.send(`
-		<input type = "text" placeholder="write something" value = "${req.query.name}" />
-		<button>Click Here</button>
-
-
-		`);
-
-});
-
-app.get('/contact',(req,res) => {
-	res.send({
-		name : 'raftaar',
-		email : 'raftaar69@gmail.com'
-	})
-});
-
+app.use(express.static(publicPath))
 app.listen(5000);
